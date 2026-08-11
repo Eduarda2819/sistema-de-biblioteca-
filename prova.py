a999c604b735923fb90fddbdacff06e1e7797bdf
Eduarda2819 <0178263@sesi-es.org.br>
2026-08-11 09:18:02 -0300

    alvar os livros na lista

--- prova.py
+++ prova.py
@@ -13,10 +13,23 @@ def carregar_livros():
             for livro in leitor:
                 livro["ano"] = int(livro["ano"])
                 livros.append(livro)
-                
+
     except FileNotFoundError:
         pass
 
     return livros
+
+# Salva todos os livros da lista no arquivo CSV.
+def salvar_livros(livros):
+    with open(ARQUIVO, "w", newline="", encoding="utf-8") as arquivo:
+        campos = ["titulo", "autor", "ano", "isbn", "status"]
+        escritor = csv.DictWriter(arquivo, fieldnames=campos)
+ 
+        escritor.writeheader()
+        escritor.writerows(livros)
+ 
+    return True
+ 
+ 
 
 
